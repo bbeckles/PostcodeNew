224660bbc7447eaf2d1f50b57f90ae8d2cbc12ce
Bailee Beckles <bbeckles@spartaglobal.com>
2019-11-08 11:32:13 +0000

    first commit

---
+++ src/test/java/com/spartaglobal/postcodeiotestservices/SinglePC/SinglePCHTTPManager/SPCHTTPManager.java
@@ -0,0 +1,4 @@
+package com.spartaglobal.postcodeiotestservices.SinglePC.SinglePCHTTPManager;
+
+public class SPCHTTPManager {
+}
---
+++ src/test/java/com/spartaglobal/postcodeiotestservices/URLConfig.java
@@ -0,0 +1,4 @@
+package com.spartaglobal.postcodeiotestservices;
+
+public class URLConfig {
+}
